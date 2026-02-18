8e2758ed300de548c9ce45de407dda7e82e22e6f
Mikael Færøvik <mikael.farovik@gmail.com>
2026-02-18 10:58:50 +0100

    hypr kb language switch functionality + powermenu sizing

--- .config/waybar/scripts/powermenu.sh
+++ .config/waybar/scripts/powermenu.sh
@@ -1,17 +1,18 @@
 #!/bin/bash
 
-entries="󰐥 Shutdown\n󰜉 Reboot\n󰤄 Sleep\n󰍃 Logout"
+options="󰐥 Power Off\n󰜉 Reboot\n󰤄 Suspend\n󰈆 Logout"
 
-selected=$(echo -e "$entries" | wofi --dmenu --cache-file /dev/null --location 3 --xoffset -10 --yoffset 2 --width 200 --lines 4 --prompt "Power Menu")
+chosen=$(echo -e "$options" | walker --dmenu --maxheight 320 | awk '{print $2}')
 
-# Execute based on selection
-case $selected in
-  *Shutdown)
-    hyprshutdown -t 'Shutting down ... ' -p 'shutdown -P 0' ;;
-  *Reboot)
-    hyprshutdown -t 'Rebooting ... ' -p 'reboot' ;;
-  *Sleep)
-    loginctl suspend ;;
-  *Logout)
-    hyprshutdown --vt 2;;
+case $chosen in
+    "Power")
+        hyprshutdown -t 'Shutting down...' --post-cmd 'shutdown -P 0' ;;
+    "Reboot")
+        hyprshutdown -t 'Restarting...' --post-cmd 'reboot' ;;
+    "Suspend")
+        systemctl suspend ;;
+    "Logout")
+        hyprshutdown -t 'Exiting...' --vt 2 ;;
+    *)
+        exit 1 ;;
 esac
